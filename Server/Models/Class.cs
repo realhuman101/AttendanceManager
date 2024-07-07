048c65daa5c9776ac13657e8bcf5c0d72e872f61
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-07 15:27:32 +0800

    Add base model classes

---
+++ Server/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Class
+    {
+        public DateTime StartTime { get; set; }
+        public string Name { get; set; }
+        public string Room { get; set; }
+    }
+}
---
+++ Server/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Class> Classes { get; set; }
+    }
+}
