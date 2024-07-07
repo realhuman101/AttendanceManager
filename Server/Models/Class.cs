d691846bcba586f144e09b71664babbc5f30a1ef
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-07 19:49:24 +0800

    Add base forgotten properties to Class class

--- Server/Models/Class.cs
+++ Server/Models/Class.cs
@@ -2,7 +2,9 @@
 {
     public class Class
     {
+        public int ID { get; set; }
         public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public string Name { get; set; }
         public string Room { get; set; }
         public int NoPeople { get; set; } // Number of people in the class
