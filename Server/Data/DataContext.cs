3b85f95703dda1ea41b89eb854dad15c9567ab5c
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-07 17:27:29 +0800

    Set up many to many relationships

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Server.Models;
+
+namespace Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ClassList> ClassLists { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Person> People { get; set; }
+    }
+}
