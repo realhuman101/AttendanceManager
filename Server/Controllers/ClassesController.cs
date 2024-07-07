661f9297dd234b49cc7e98d79e9377054b7aec01
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-07 22:07:44 +0800

    add classes endpoint sect

---
+++ Server/Controllers/ClassesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Server.Interfaces;
+using Server.Models;
+using Server.Repository;
+
+namespace Server.Controllers
+{
+    [Route("/api/classes/[controller]")]
+    [ApiController]
+    public class ClassesController : Controller
+    {
+        private readonly IRepository<Class> _classesRepository;
+
+        public ClassesController(IRepository<Class> classRepository)
+        {
+            _classesRepository = classRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<ClassesRepository>))]
+        public IActionResult GetClasses()
+        {
+            var classes = _classesRepository.Get();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(classes);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(ClassesRepository))]
+        public IActionResult GetClass(int id)
+        {
+            var _class = _classesRepository.GetByID(id);
+
+            if (_class == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_class);
+        }
+    }
+}
