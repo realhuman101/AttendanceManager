cf2b8361932102b7e99c44b07927852e8b242f40
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-12 22:06:32 +0800

    add login page

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Login = () => {
   return (
 	<div id="login">
+		<h1>Log In</h1>
+		<form action="">
+			<label htmlFor="email">Enter your email: </label>
+			<input type="email" name="email" id="email" placeholder="your@email.com" required />
+			
+			<label htmlFor="Password">Enter your password: </label>
+			<input type="password" name="password" id="password" placeholder="Password" required />
 
+			<input type="submit" value="Log In" />
+		</form>
 	</div>
   )
 }
